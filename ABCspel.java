b2da9d32887383e98a57bbcf8fb8f39d856d9884
Tim Kroon <timkroon0703@gmail.com>
2019-02-23 16:43:24 +0100

    ABCspel

---
+++ ABCspel.java
@@ -0,0 +1,196 @@
+import java.util.Scanner;
+
+public class ABCspel {
+	public static void main(String [] args) {
+		System.out.println("Voer het eerste letter van het alfabet in.");
+		
+		String whileloop = "a";
+		Scanner invoer = new Scanner(System.in);
+		
+		while(whileloop == "a") {
+			
+			String ingegevena = invoer.nextLine();
+			if(ingegevena.equals("a")) {
+				System.out.println("a, en nu?");	
+			}else {
+				System.out.println("Dat is niet de eerste letter, bein opnieuw.");
+			}
+			
+			String ingegevenb = invoer.nextLine();
+			if(ingegevenb.equals("b")) {
+				System.out.println("b, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenc = invoer.nextLine();
+			if(ingegevenc.equals("c")) {
+				System.out.println("c, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevend = invoer.nextLine();
+			if(ingegevend.equals("d")) {
+				System.out.println("d, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevene = invoer.nextLine();
+			if(ingegevene.equals("e")) {
+				System.out.println("e, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenf = invoer.nextLine();
+			if(ingegevenf.equals("f")) {
+				System.out.println("f, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegeveng = invoer.nextLine();
+			if(ingegeveng.equals("g")) {
+				System.out.println("g, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenh = invoer.nextLine();
+			if(ingegevenh.equals("h")) {
+				System.out.println("h, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegeveni = invoer.nextLine();
+			if(ingegeveni.equals("i")) {
+				System.out.println("i, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenj = invoer.nextLine();
+			if(ingegevenj.equals("j")) {
+				System.out.println("j, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenk = invoer.nextLine();
+			if(ingegevenk.equals("k")) {
+				System.out.println("k, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenl = invoer.nextLine();
+			if(ingegevenl.equals("l")) {
+				System.out.println("l, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenm = invoer.nextLine();
+			if(ingegevenm.equals("m")) {
+				System.out.println("m, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenn = invoer.nextLine();
+			if(ingegevenn.equals("n")) {
+				System.out.println("n, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegeveno = invoer.nextLine();
+			if(ingegeveno.equals("o")) {
+				System.out.println("o, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenp = invoer.nextLine();
+			if(ingegevenp.equals("p")) {
+				System.out.println("p, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenq = invoer.nextLine();
+			if(ingegevenq.equals("q")) {
+				System.out.println("q, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenr = invoer.nextLine();
+			if(ingegevenr.equals("r")) {
+				System.out.println("r, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevens = invoer.nextLine();
+			if(ingegevens.equals("s")) {
+				System.out.println("s, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevent = invoer.nextLine();
+			if(ingegevent.equals("t")) {
+				System.out.println("t, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenu = invoer.nextLine();
+			if(ingegevenu.equals("u")) {
+				System.out.println("u, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenv = invoer.nextLine();
+			if(ingegevenv.equals("v")) {
+				System.out.println("v, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenw = invoer.nextLine();
+			if(ingegevenw.equals("w")) {
+				System.out.println("w, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenx = invoer.nextLine();
+			if(ingegevenx.equals("x")) {
+				System.out.println("x, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegeveny = invoer.nextLine();
+			if(ingegeveny.equals("y")) {
+				System.out.println("y, en nu?");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+			
+			String ingegevenz = invoer.nextLine();
+			if(ingegevenz.equals("z")) {
+				System.out.println("HOERA!! Wat kan jij dit goed zeg!");
+			}else {
+				System.out.println("dat is niet de volgende letter, bein opnieuw");
+			}
+
+		}
+	}
+}
